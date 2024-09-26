a7a0d002924b94537f929b712eef6cff197ebe87
Jxvyz <jayrabbit@gmail.com>
2024-09-25 21:56:52 -0500

    condicional

---
+++ condicionalIf.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+
+void main(void) {
+	int numeroC = 0;
+
+	printf("Numero de cuenta: ", numeroC);
+	scanf_s("%i", &numeroC);
+
+	if (numeroC == 322157615 || numeroC == 322246997) {
+		printf("Llegaron los top globales");
+	}
+	else {
+		printf("Quien sos?, peñagod infiltrado?");
+	}
+}
